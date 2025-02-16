d69327c71684cfa513a7068d728cae0702aa124c
loqatis <marc.serra@enti.cat>
2025-02-16 11:25:43 +0100

    Camera Flash

--- Assets/NewProject/scripts/cameraManager.cs
+++ Assets/NewProject/scripts/cameraManager.cs
@@ -1,19 +1,31 @@
 using System.Collections;
 using System.Timers;
 using UnityEngine;
+using UnityEngine.Rendering;
 
-public class cameraFeedbackManager : MonoBehaviour
+public class cameraManager : MonoBehaviour
 {
+    VolumeProfile volumeProfile;
+    UnityEngine.Rendering.Universal.Vignette colorPostProces;
     Vector3 originalPosition;
     float elapsed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         originalPosition = transform.position;
+        
+        volumeProfile = GameObject.Find("Global Volume").GetComponent<Volume>()?.profile;
+        if (!volumeProfile) throw new System.NullReferenceException(nameof(VolumeProfile));
+        
+        if (!volumeProfile.TryGet(out colorPostProces)) throw new System.NullReferenceException(nameof(colorPostProces));
 
+
+        
     }
 
-    public IEnumerator CameraShake(float timeLenght, float range)
+
+
+    public IEnumerator Shake(float timeLenght, float range)
     {
         elapsed = 0.0f;
         while (elapsed < timeLenght)
@@ -27,5 +39,23 @@ public class cameraFeedbackManager : MonoBehaviour
 
         transform.position = originalPosition;
 
+    }
+
+    public IEnumerator Flash(float intensity, float length, Color color)
+    {
+        Debug.Log("Color set");
+        colorPostProces.color.Override(color);
+        colorPostProces.intensity.Override(intensity);
+        elapsed = 0.0f;
+        while (elapsed < length)
+        {
+            colorPostProces.intensity.Override(intensity-=0.2f);
+
+            elapsed += Time.deltaTime;
+            Debug.Log(colorPostProces.intensity);
+            yield return null;
+        }
+        colorPostProces.intensity.Override(0);
+        yield return null;
     }
 }
