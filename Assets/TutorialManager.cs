33a0339ee077a2f89ebd298cf4132aa81356c6f5
JanPlanes <jan.planes@enti.cat>
2025-02-20 12:31:29 +0100

    Start sequence script

---
+++ Assets/TutorialManager.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject[] popUps;
+
+    private int popUpIndex;
+
+    private void Update()
+    {
+        for(int i = 0; i < popUps.Length; i++)
+        {
+            if(i==popUpIndex)
+            {
+                popUps[popUpIndex].SetActive(true);
+            } 
+            else
+            {
+                popUps[popUpIndex].SetActive(false);
+            }
+        }
+          if(popUpIndex == 0)
+          {
+            if(Input.GetMouseButton(0))
+            {
+                Debug.Log("NIGGA");
+            }
+          }
+    }
+}
