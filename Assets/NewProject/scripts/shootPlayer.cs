f564238b9cbbfc5e6fcd1ec2d6fa8a52b35b9842
loqatis <marc.serra@enti.cat>
2025-03-12 11:54:24 +0100

    Merge fixes

--- Assets/NewProject/scripts/shootPlayer.cs
+++ Assets/NewProject/scripts/shootPlayer.cs
@@ -53,7 +53,7 @@ public class shootPlayer : MonoBehaviour
         GameObject bullet = Instantiate(bulletPrefab, _playerPosition, Quaternion.identity);
 
         
-        bullet.GetComponent<multiShoot>().setShootDirection(shootDirection,true);
+        bullet.GetComponent<DestroyBullet>().setShootDirection(shootDirection,true);
 
         
 
