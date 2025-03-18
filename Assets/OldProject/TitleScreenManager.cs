384f2b6c6182002f42c480440bbe8fdd6ce3cdac
JAN PLANES MAZCUÑAN <jan.planes@enti.cat>
2025-03-18 21:50:33 +0100

    Slight visual fix of Title Screen

--- Assets/OldProject/TitleScreenManager.cs
+++ Assets/OldProject/TitleScreenManager.cs
@@ -6,33 +6,54 @@ public class TitleScreenManager : MonoBehaviour
 {
     [SerializeField] private Canvas settings;
     [SerializeField] private Canvas titleScreen;
+    [SerializeField] private Canvas creditsScreen;
     public void StartGame()
     {
         SceneManager.LoadScene("Node Map");
     }
     public void BootSettings()
     {
+        creditsScreen.enabled = false;
         settings.enabled = true;
         titleScreen.enabled = false;
+    }
+    public void BootCredits()
+    {
+        creditsScreen.enabled = true;
+        settings.enabled = false;
+        titleScreen.enabled = false;
+    }
+
+    public void ExitCredits()
+    {
+        if (creditsScreen.enabled)
+        {
+            settings.enabled = false;
+            creditsScreen.enabled = false;
+            titleScreen.enabled = true;
+        }
+    } 
+    public void ExitSettings()
+    {
+        if (settings.enabled)
+        {
+            settings.enabled = false;
+            creditsScreen.enabled = false;
+            titleScreen.enabled = true;
+        }
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         settings.enabled = false;
+        creditsScreen.enabled = false;
         titleScreen.enabled = true;
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyUp(KeyCode.Escape))
-        {
-            if(settings.enabled)
-            {
-                settings.enabled = false;
-                titleScreen.enabled = true;
-            }
-        }
+        
     }
     public void CloseGame()
     {
