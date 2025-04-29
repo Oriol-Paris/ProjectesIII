ff559128d5fb701c0567b7c37f8200e334e27fc2
raul <raul.velez@enti.cat>
2025-04-29 15:14:41 +0200

    new mouse

--- Assets/MouseIcon.cs
+++ Assets/MouseIcon.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class MouseIcon : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject indicatorPrefab;
+    public LayerMask groundLayer;
+    private GameObject currentIndicator;
+    public Vector3 mov;
+
     void Start()
     {
-        
+        if (indicatorPrefab != null)
+        {
+            currentIndicator = Instantiate(indicatorPrefab);
+            
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, 100f, groundLayer))
+        {
+            if (currentIndicator != null)
+            {
+                currentIndicator.SetActive(true);
+                currentIndicator.transform.position = (hit.point - mov) + Vector3.up * 0.01f; // un poco elevado para evitar z-fighting
+                currentIndicator.transform.rotation = Quaternion.Euler(90f, 0f, 0f); // aplanado si es un sprite
+            }
+        }
     }
+
 }
