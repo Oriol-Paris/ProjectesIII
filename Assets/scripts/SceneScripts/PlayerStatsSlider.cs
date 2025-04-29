5245ed75661a36aeae1cede34762de61f62e7e4b
Oriol-Paris <oriol.paris@enti.cat>
2025-04-29 20:41:50 +0200

    Shop Finished

--- Assets/scripts/SceneScripts/PlayerStatsSlider.cs
+++ Assets/scripts/SceneScripts/PlayerStatsSlider.cs
@@ -6,23 +6,29 @@ public class PlayerStatsSlider : MonoBehaviour
     Vector3 startPos;
     public Vector3 endPos;
     public float speed = 1f;
+    public GameObject blurBG;
+    public GameObject exitText;
 
     private void Start()
     {
         startPos = transform.position;
+        blurBG.SetActive(false);
+        exitText.SetActive(false);
     }
 
     public void SlideOut()
     {
-        StartCoroutine(LerpMovement(startPos, endPos));
+        StartCoroutine(LerpMovement(startPos, endPos, true));
     }
 
     public void SlideIn()
     {
-        StartCoroutine(LerpMovement(endPos, startPos));
+        StartCoroutine(LerpMovement(endPos, startPos, false));
+        blurBG.SetActive(false);
+        exitText.SetActive(false);
     }
 
-    IEnumerator LerpMovement(Vector3 start, Vector3 end)
+    IEnumerator LerpMovement(Vector3 start, Vector3 end, bool outMov)
     {
         float elapsedTime = 0f;
         while (elapsedTime < speed)
@@ -32,5 +38,11 @@ public class PlayerStatsSlider : MonoBehaviour
             yield return null;
         }
         transform.position = end;
+
+        if(outMov)
+        {
+            blurBG.SetActive(true);
+            exitText.SetActive(true);
+        }
     }
 }
