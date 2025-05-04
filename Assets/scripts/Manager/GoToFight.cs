7e74e112f2060306ef37cdbf16f95dbed31be9be
Oriol-Paris <oriol.paris@enti.cat>
2025-05-04 22:46:15 +0200

    UI SFX

--- Assets/scripts/Manager/GoToFight.cs
+++ Assets/scripts/Manager/GoToFight.cs
@@ -1,10 +1,21 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GoToFight : MonoBehaviour
 {
+    [SerializeField] private AudioSource enterSfxSource;
+
     public void NextRound()
     {
+        StartCoroutine(NextRoundwSfx());
+    }
+
+    private IEnumerator NextRoundwSfx()
+    {
+        enterSfxSource.Play();
+        yield return new WaitForSeconds(enterSfxSource.clip.length);
+
         PlayerPrefs.SetString("LastLevelCleared", SceneManager.GetActiveScene().name);
         SceneManager.LoadScene("Node Map 2.0");
     }
