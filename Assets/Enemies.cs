6567cec92a5243618c7d062f483c0344cca94b7f
raul <raul.velez@enti.cat>
2025-05-12 19:36:04 +0200

    1try

--- Assets/Enemies.cs
+++ Assets/Enemies.cs
@@ -1,14 +1,16 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI; // Importante para usar NavMesh
-using UnityEngine.Rendering;
-using UnityEngine.UIElements;
+using UnityEngine.AI;
 
-public class EnemyMovement : MonoBehaviour
+public class Enemies : MonoBehaviour
 {
-    #region VARIABLES
+    public float speed;
+    public float rango;
+    public float preAttack;
+    public float timeAttack;
+    public float postAttack;
+    public bool isAttacking = false; 
+    public bool isSleeping = false;
+
 
     [SerializeField] private TimeSecuence Player;
     private Vector3 PlayerPos;
@@ -16,18 +18,16 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField] private float velocity;
     [SerializeField] private float range;
     [SerializeField] private float restTime = 1.5f; // Tiempo de descanso tras un ataque
-   
-    [SerializeField]private NavMeshAgent agent; // Referencia al NavMeshAgent
-    [SerializeField]private GameObject attack; 
+
+    [SerializeField] private NavMeshAgent agent; // Referencia al NavMeshAgent
+    [SerializeField] private GameObject attack;
     private SpriteRenderer spriteRenderer; // Referencia al SpriteRenderer
-    private bool isResting = false; // Indica si el enemigo estÃ¡ en descanso
+    private bool isResting = false; // Indica si el enemigo está en descanso
     [SerializeField] private float stopDistance = 1.0f; // Distance to stop from the player
 
     [SerializeField] AudioClip[] attackClips;
 
-    #endregion
 
-    public enum ActionEnum { MOVE, SHOOT, HEAL, MELEE, NOTHING };
 
     void Start()
     {
@@ -37,21 +37,22 @@ public class EnemyMovement : MonoBehaviour
         agent.speed = velocity * PlayerPrefs.GetFloat("DifficultyMultiplier");
         range *= PlayerPrefs.GetFloat("DifficultyMultiplier");
         agent.updateRotation = false;
-       
+
         spriteRenderer = GetComponent<SpriteRenderer>(); // Obtener el componente SpriteRenderer
     }
 
+   
     void Update()
     {
-        if (enemyStats.isAlive) // No moverse si estÃ¡ descansando
+        if (enemyStats.isAlive) // No moverse si está descansando
         {
             PlayerPos = Player.transform.position;
             float distanceToPlayer = Vector3.Distance(transform.position, PlayerPos);
 
-            // Voltear sprite dependiendo de la posiciÃ³n del jugador
+            // Voltear sprite dependiendo de la posición del jugador
             spriteRenderer.flipX = PlayerPos.x < transform.position.x;
 
-           
+
 
             if (Player.GetIsExecuting() || Player.GetComponent<PlayerBase>().GetInAction())
             {
@@ -69,18 +70,74 @@ public class EnemyMovement : MonoBehaviour
             }
             else
             {
-              
+
                 GetComponent<Animator>().SetBool("isMoving", false);
                 agent.isStopped = true;
                 agent.velocity = Vector3.zero;
-               
+
             }
         }
         else
         {
-            agent.speed = 0;    
+            agent.speed = 0;
         }
     }
 
-  
+    IEnumerator AttackCoroutine()
+    {
+        isResting = true; // Comienza el descanso
+        Debug.Log("Attacking");
+        GetComponent<Animator>().SetTrigger("attack");
+
+        GameObject _attack = Instantiate(attack, transform.position, Quaternion.identity, transform);
+        yield return new WaitForSeconds(0.5f);
+
+        Destroy(_attack);
+
+        yield return new WaitForSeconds(restTime);
+
+        isResting = false;
+    }
+
+    public void Attack()
+    {
+        if (!isResting) // Evita que ataque si aún está descansando
+        {
+            StartCoroutine(AttackCoroutine());
+            SoundEffectsManager.instance.PlaySoundFXClip(attackClips, transform, 1f);
+        }
+    }
+
+
+    public IEnumerator Shake(float timeLenght, float range)
+    {
+        float elapsed = 0.0f;
+        while (elapsed < timeLenght)
+        {
+            UnityEngine.Vector3 movingPosition = UnityEngine.Vector3.zero;
+
+            float x = 0;
+            float z = 0;
+
+            x = Random.Range(originalPosition.x - 1.0f * range, originalPosition.x + 1.0f * range);
+            z = Random.Range(originalPosition.z - 1.0f * (range / 2), originalPosition.z + 1.0f * (range / 2));
+
+            //Debug.Log(originalPosition);
+            movingPosition.x = x;
+            movingPosition.y = transform.position.y;
+            movingPosition.z = z;
+
+            transform.position = movingPosition;
+
+            elapsed += Time.deltaTime;
+
+
+
+            yield return null;
+
+        }
+
+        transform.position = originalPosition;
+
+    }
 }
